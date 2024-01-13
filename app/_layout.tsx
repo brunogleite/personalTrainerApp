aeaee6168262e116d69d5f4868a7986e9fd72832
Bruno Leite <brunoleite6o@gmail.com>
2024-01-13 17:09:15 +0000

    complete Drawer layout file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
-import { Stack } from 'expo-router';
+import { Slot, Stack } from 'expo-router';
 import { TamaguiProvider } from 'tamagui';
 
 import config from '../tamagui.config';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function Layout() {
   return (
     <TamaguiProvider config={config}>
-      <Stack />
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Slot />
+      </GestureHandlerRootView>
     </TamaguiProvider>
   );
 }
