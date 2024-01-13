e7e07397eac93ee6d85811fa55a155eb4def9934
Bruno Leite <brunoleite@N32WM22Q9Y.local>
2024-01-13 00:24:02 +0000

    add expo-router/drawer package

---
+++ app/(drawer)/_layout.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { Drawer } from 'expo-router/drawer'
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <Drawer />
+  )
+}
+
+export default Layout
+
+const styles = StyleSheet.create({})
