559fd04536b20dac09a71073f8721fa1bbc571cd
Bruno Leite <brunoleite6o@gmail.com>
2024-01-13 17:10:35 +0000

    install tanstack query package

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,37 @@
-import { Slot, Stack } from 'expo-router';
-import { TamaguiProvider } from 'tamagui';
+import { useFonts } from 'expo-font';
+import { SplashScreen, Slot } from 'expo-router';
+import React, { useEffect } from 'react';
+import { TamaguiProvider, Theme } from 'tamagui';
 
 import config from '../tamagui.config';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { queryClient } from '@/queryClient';
+import { QueryClientProvider } from '@tanstack/react-query';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) return null;
+
   return (
     <TamaguiProvider config={config}>
       <GestureHandlerRootView style={{ flex: 1 }}>
-        <Slot />
+        <QueryClientProvider client={queryClient}>
+          <Theme name={'blue'}>
+            <Slot />
+          </Theme>
+        </QueryClientProvider>
       </GestureHandlerRootView>
     </TamaguiProvider>
   );
